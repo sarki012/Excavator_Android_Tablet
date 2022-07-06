3a8fc0ec98558906412b5f0b4730af112d00a351
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-07-06 16:17:12 -0500

    Working Red Spherical Joysticks Multitouch
    Joysticks don't stay in bounds yet.

--- Excavator/Assets.java
+++ Excavator/Assets.java
@@ -4,6 +4,7 @@ import com.esark.framework.Pixmap;
 
 public class Assets {
     public static Pixmap excavatorTabletLandscapeBackground;
+    public static Pixmap redJoystick;
     public static Pixmap excavatorPortraitBackground;
 }
 
